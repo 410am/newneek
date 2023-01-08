ad3f56aaaea5a480ab583d2ad52010784f506b69
410am <rlclqk@naver.com>
2023-01-09 02:18:13 +0900

    너무 졸려

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import './App.css';
 import Main from './components/Main'
-import Nav from './components/Nav'
-// `#EAE7DE`
+
 function App() {
 
   return (
-    //TODO: 뷰포트 전체의 색상 설정하기
-    // background-color='#EAE7DE'
-    <body>
-      <div className='App'>
-        <Main />
-      </div>
-    </body>
+    <div className='App bg-[#eae7de]'>
+      <Main />
+    </div>
   );
 }
 
